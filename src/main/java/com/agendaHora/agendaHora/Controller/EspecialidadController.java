3f045e040f2b99e854004b7a11b107eb00979eb2
mndiazf <mn.diaz.f@gmail.com>
2023-11-25 06:14:55 -0300

    test-cors

--- src/main/java/com/agendaHora/agendaHora/Controller/EspecialidadController.java
+++ src/main/java/com/agendaHora/agendaHora/Controller/EspecialidadController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@CrossOrigin(origins = "*", allowedHeaders = "*")
+@CrossOrigin(origins = "https://playerdos.com", allowedHeaders = "*")
 @RestController
 @RequestMapping("/api/especialidades")
 public class EspecialidadController {
